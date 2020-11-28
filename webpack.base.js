12b058d0243c01a89205df4047e3a746472c0de9
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-11-28 15:56:00 +0100

    Bump version

--- webpack.base.js
+++ webpack.base.js
@@ -109,7 +109,10 @@ module.exports = {
     mode: 'production',
     plugins: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': 'production'
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            },
+            'production': 'production'
         }),
         //new CleanWebpackPlugin(),
         new HardSourceWebpackPlugin(),
